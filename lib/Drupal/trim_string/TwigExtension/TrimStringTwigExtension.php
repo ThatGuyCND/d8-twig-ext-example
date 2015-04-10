96e44c7d4ec3413b9e73cbf0c8cd97105fb4719b
Erik Baldwin <erik.baldwin@acquia.com>
2015-04-10 16:29:13 -0400

    added logic for trim_string

--- lib/Drupal/trim_string/TwigExtension/TrimStringTwigExtension.php
+++ lib/Drupal/trim_string/TwigExtension/TrimStringTwigExtension.php
@@ -1,14 +1,31 @@
 <?php
 
-namespce Drupal\trim_string\TwigExtension;
-
+namespace Drupal\trim_string\TwigExtension;
 
 use Drupal\Core\Template\TwigExtension;
 
-class trimStringTwigExtension extends TwigExtension {
+class TrimStringTwigExtension extends \Twig_Extension {
 
   public function getName() {
     return 'trim_string.trim_string_twig_extension';
+  }
+
+  public function getFilters() {
+    return array(
+      new \Twig_SimpleFilter( array('Drupal\trim_string\TwigExtension\trimStringTwigExtension', 'trimString') )
+    );
+  }
+
+  public static function trimString ($string, $limit = 10, $suffix = '&hellip;') {
+    $words = explode(' ', $string);
+
+    if ( count($words) < $limit ) {
+      return $string;
+    }
+
+    $string = implode(' ', array_slice($words, 0, $limit)) . $suffix;
+
+    return $string;
   }
 
 }
